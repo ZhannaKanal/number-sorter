338a722916db17f066558d57b4d8e410a047da9d
Zhanna <kezhanna.03@mail.ru>
2025-12-08 23:42:47 +0500

    task 31

--- script.js
+++ script.js
@@ -36,16 +36,20 @@ const bubbleSort = (array) => {
 const selectionSort = (array) => {
   for (let i = 0; i < array.length; i++) {
     let minIndex = i;
+
     for (let j = i + 1; j < array.length; j++) {
       console.log(array, array[j], array[minIndex]);
       if (array[j] < array[minIndex]) {
         minIndex = j;
       }
     }
+
     const temp = array[i];
     array[i] = array[minIndex];
     array[minIndex] = temp;
   }
+
+  return array;
 }
 
 sortButton.addEventListener("click", sortInputArray);
