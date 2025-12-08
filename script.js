f9d31216ebce3f9786830fb41c8f24dd21ae65bf
Zhanna <kezhanna.03@mail.ru>
2025-12-08 21:15:15 +0500

    task 25

--- script.js
+++ script.js
@@ -22,7 +22,6 @@ const updateUI = (array = []) => {
 const bubbleSort = (array) => {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length - 1; j++) {
-
       if (array[j] > array[j + 1]) {
         const temp = array[j];
         array[j] = array[j + 1];
@@ -31,7 +30,11 @@ const bubbleSort = (array) => {
     }
   }
 
-return array;
+  return array;
 }
+
+const selectionSort = (array) => {
+
+};
 
 sortButton.addEventListener("click", sortInputArray);
